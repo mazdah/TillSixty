b2009d777ccbe34c9d5ad27e82edf8d0bebca1a7
WOO HYOUNG JUN <woohj70@gmail.com>
2016-10-01 15:34:28 +0900

    최초 커

---
+++ src/main/java/com/mazdah/tillsixty/Till60Application.java
@@ -0,0 +1,12 @@
+package com.mazdah.tillsixty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Till60Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Till60Application.class, args);
+	}
+}
---
+++ src/test/java/com/mazdah/tillsixty/Till60ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mazdah.tillsixty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Till60ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
