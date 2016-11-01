b72478b22a9ba0912731a73c36cb39fef09ad4af
Mazdah <woohj70@gmail.com>
2016-11-01 11:08:22 +0900

    로그인 페이지 연결을 위한 수정

--- src/main/java/com/mazdah/tillsixty/controller/login/TSLoginController.java
+++ src/main/java/com/mazdah/tillsixty/controller/login/TSLoginController.java
@@ -1,5 +1,7 @@
 package com.mazdah.tillsixty.controller.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/ts/login")
 public class TSLoginController {
 
+	private static final Logger logger = LoggerFactory.getLogger(TSLoginController.class);
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public String goLogin() {
+		logger.debug("##### goLogin");
+		
 		return "/login/login";
 	}
 }
