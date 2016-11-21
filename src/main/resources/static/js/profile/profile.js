ddb328f4883df610f8dc5dc9c362d6c8f3888995
Mazdah <woohj70@gmail.com>
2016-11-21 16:32:28 +0900

    chart 샘플 구현

--- src/main/resources/static/js/profile/profile.js
+++ src/main/resources/static/js/profile/profile.js
@@ -199,12 +199,220 @@ $('document').ready(function () {
 	
 	view.setProfile();
 	view.setGoal();
+	view.setRadarChart();
+	view.setTodayStatChart();
+	view.setElementsStatChart();
+	view.setRangeStatChart();
 });
 
 var view = function () {
 	var _init = function () {
 		
 	};
+	
+	var _setRadarChart = function () {
+		//////////////////////////////////////////////////////////////
+		//////////////////////// Set-Up ////////////////////////////// 
+		////////////////////////////////////////////////////////////// 
+		var margin = {top: 65, right: 60, bottom: 45, left: 60},
+			width = Math.min(420, window.innerWidth - 10) - margin.left - margin.right,
+			height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
+				
+		//alert(width + ' : ' + height);
+		////////////////////////////////////////////////////////////// 
+		////////////////////////// Data ////////////////////////////// 
+		////////////////////////////////////////////////////////////// 
+		var data = [
+				  [//iPhone
+					{axis:"Idea",value:0.22},
+					{axis:"Info",value:0.28},
+					{axis:"Resource",value:0.29},
+					{axis:"Mentor",value:0.17},
+					{axis:"Risk",value:0.22}		
+				  ]
+				];
+		////////////////////////////////////////////////////////////// 
+		//////////////////// Draw the Chart ////////////////////////// 
+		////////////////////////////////////////////////////////////// 
+		var color = d3.scale.ordinal().range(["#EDC951","#CC333F","#00A0B0"]);
+			
+		var radarChartOptions = {
+		  w: width,
+		  h: height,
+		  margin: margin,
+		  maxValue: 0.5,
+		  levels: 5,
+		  roundStrokes: true,
+		  color: color
+		};
+		//Call function to draw the Radar chart
+		RadarChart(".radarChart", data, radarChartOptions);
+	};
+	
+	var _setTodayStatChart = function () {
+		var historicalBarChart = [
+		      		            {
+		      		                key: "Cumulative Return",
+		      		                values: [
+		      		                  { 
+		      		                    "label" : "Idea" ,
+		      		                    "value" : 1
+		      		                  } , 
+		      		                  { 
+		      		                    "label" : "Resource" , 
+		      		                    "value" : 0
+		      		                  } , 
+		      		                  { 
+		      		                    "label" : "Info" , 
+		      		                    "value" : 2
+		      		                  } , 
+		      		                  { 
+		      		                    "label" : "Mentor" , 
+		      		                    "value" : 0
+		      		                  } , 
+		      		                  { 
+		      		                    "label" : "Risk" ,
+		      		                    "value" : 0
+		      		                  }
+		      		                ]
+		      		              }
+		      		            ]
+		      		
+		      		nv.addGraph(function() {
+		      	        var chart = nv.models.discreteBarChart()
+		      	            .x(function(d) { return d.label })
+		      	            .y(function(d) { return d.value })
+		      	            .staggerLabels(false)
+		      	            //.staggerLabels(historicalBarChart[0].values.length > 8)
+		      	            .showValues(true)
+		      	            .duration(250)
+		      	            ;
+
+		      	        //chart.yAxis.tickFormat(d3.format(',.0%'));
+		      	        
+		      	        d3.select('#_today-chart svg')
+		      	            .datum(historicalBarChart)
+		      	            .call(chart);
+
+		      	        nv.utils.windowResize(chart.update);
+		      	        return chart;
+		      	    });
+	};
+	
+	var _setElementsStatChart = function () {
+		var historicalBarChart = [
+		            {
+		                key: "Cumulative Return",
+		                values: [
+		                  { 
+		                    "label" : "Idea" ,
+		                    "value" : 5
+		                  } , 
+		                  { 
+		                    "label" : "Resource" , 
+		                    "value" : 3
+		                  } , 
+		                  { 
+		                    "label" : "Info" , 
+		                    "value" : 7
+		                  } , 
+		                  { 
+		                    "label" : "Mentor" , 
+		                    "value" : 1
+		                  } , 
+		                  { 
+		                    "label" : "Risk" ,
+		                    "value" : 2
+		                  }
+		                ]
+		              }
+		            ]
+		
+		nv.addGraph(function() {
+	        var chart = nv.models.discreteBarChart()
+	            .x(function(d) { return d.label })
+	            .y(function(d) { return d.value })
+	            .staggerLabels(false)
+	            //.staggerLabels(historicalBarChart[0].values.length > 8)
+	            .showValues(true)
+	            .duration(250)
+	            ;
+
+	        //chart.yAxis.tickFormat(d3.format(',.0%'));
+	        
+	        d3.select('#_elements-chart svg')
+	            .datum(historicalBarChart)
+	            .call(chart);
+
+	        nv.utils.windowResize(chart.update);
+	        return chart;
+	    });
+	};
+	
+	var _setRangeStatChart = function () {
+		var data = [
+		             {
+		                key: "Idea",
+		                values: [ [ 1083297600000 , -2.974623048543] , [ 1085976000000 , -1.7740300785979] , [ 1088568000000 , 4.4681318138177] , [ 1091246400000 , 7.0242541001353] , [ 1093924800000 , 7.5709603667586] , [ 1096516800000 , 20.612245065736] , [ 1099195200000 , 21.698065237316] , [ 1101790800000 , 40.501189458018] , [ 1104469200000 , 50.464679413194]]
+		                ,
+		                mean: 250
+		            },
+		            {
+		                key: "Resource",
+		                values: [ [ 1083297600000 , -0.77078283705125] , [ 1085976000000 , -1.8356366650335] , [ 1088568000000 , -5.3121322073127] , [ 1091246400000 , -4.9320975829662] , [ 1093924800000 , -3.9835408823225] , [ 1096516800000 , -6.8694685316805] , [ 1099195200000 , -8.4854877428545] , [ 1101790800000 , -15.933627197384] , [ 1104469200000 , -15.920980069544]]
+		                ,
+		                mean: -60
+		            },
+		            {
+		                key: "Info",
+		                mean: 125,
+		                values: [ [ 1083297600000 , -3.7454058855943] , [ 1085976000000 , -3.6096667436314] , [ 1088568000000 , -0.8440003934950] , [ 1091246400000 , 2.0921565171691] , [ 1093924800000 , 3.5874194844361] , [ 1096516800000 , 13.742776534056] , [ 1099195200000 , 13.212577494462] , [ 1101790800000 , 24.567562260634] , [ 1104469200000 , 34.543699343650]]
+		            },
+		            {
+		                key: "Mentor",
+		                values: [ [ 1083297600000 , -1.7798428181819] , [ 1085976000000 , -0.36883324836999] , [ 1088568000000 , 1.7312581046040] , [ 1091246400000 , -1.8356125950460] , [ 1093924800000 , -1.5396564170877] , [ 1096516800000 , -0.16867791409247] , [ 1099195200000 , 1.3754263993413] , [ 1101790800000 , 5.8171640898041] , [ 1104469200000 , 9.4350145241608]]
+		            },
+		            {
+		                key: "Risk",
+		                values: [ [ 1083297600000 , 1.7798428181819] , [ 1085976000000 , 0.36883324836999] , [ 1088568000000 , 2.7312581046040] , [ 1091246400000 , 3.8356125950460] , [ 1093924800000 , 4.5396564170877] , [ 1096516800000 , 5.16867791409247] , [ 1099195200000 , 5.3754263993413] , [ 1101790800000 , 5.8171640898041] , [ 1104469200000 , 9.4350145241608]]
+		            }
+		        ];
+		
+		nv.addGraph(function() {
+	        var chart = nv.models.cumulativeLineChart()
+	            .useInteractiveGuideline(true)
+	            .x(function(d) { return d[0] })
+	            .y(function(d) { return d[1]/100 })
+	            .color(d3.scale.category10().range())
+	            .average(function(d) { return d.mean/100; })
+	            .duration(300)
+	            .clipVoronoi(false)
+	            .showControls(false);
+	        chart.dispatch.on('renderEnd', function() {
+	            console.log('render complete: cumulative line with guide line');
+	        });
+
+	        chart.xAxis.tickFormat(function(d) {
+	            return d3.time.format('%m/%d/%y')(new Date(d))
+	        });
+
+	        chart.yAxis.tickFormat(d3.format(',.0%'));
+
+	        d3.select('#_range_chart svg')
+	            .datum(data)
+	            .call(chart);
+
+	        //TODO: Figure out a good way to do this automatically
+	        nv.utils.windowResize(chart.update);
+
+	        chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+	        chart.state.dispatch.on('change', function(state){
+	            nv.log('state', JSON.stringify(state));
+	        });
+
+	        return chart;
+	    });
+	}
 	
 	var _setProfile = function () {
 		var fbObj = $('._profile_facebook');
@@ -285,10 +493,14 @@ var view = function () {
 	};
 	
 	return {
-		init		: _init,
-		setProfile	: _setProfile,
-		setGoal		: _setGoal,
-		setPrev	: _setPrev
+		init					: _init,
+		setTodayStatChart		: _setTodayStatChart,
+		setRadarChart			: _setRadarChart,
+		setElementsStatChart	: _setElementsStatChart,
+		setRangeStatChart		: _setRangeStatChart,
+		setProfile				: _setProfile,
+		setGoal					: _setGoal,
+		setPrev					: _setPrev
 	}
 }();
 view.init();
