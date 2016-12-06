c7d3f569b425e4e7762f3608dbe6208ff8c9eba8
Mazdah <woohj70@gmail.com>
2016-12-06 10:45:05 +0900

    패키지명 교체

---
+++ src/main/java/com/mazdah/tillsixty/login/controller/TSLoginController.java
@@ -0,0 +1,21 @@
+package com.mazdah.tillsixty.login.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/ts/login")
+public class TSLoginController {
+
+	private static final Logger logger = LoggerFactory.getLogger(TSLoginController.class);
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String goLogin() {
+		logger.debug("##### goLogin");
+		
+		return "/login/login";
+	}
+}
