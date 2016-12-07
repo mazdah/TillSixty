bf4a78c3facf21a2cf5f016596d6e51cf0af1132
Mazdah <woohj70@gmail.com>
2016-12-07 18:21:51 +0900

    @RepositoryRestResource 테스트를 위한 domain 클래스 및 repository 인터페이스 생성

--- src/main/java/com/mazdah/tillsixty/account/domain/ProfileRepository.java
+++ src/main/java/com/mazdah/tillsixty/account/domain/ProfileRepository.java
@@ -1,5 +1,13 @@
 package com.mazdah.tillsixty.account.domain;
 
-public interface ProfileRepository {
+import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProfileRepository extends MongoRepository<Profile, String> {
+
+	List<Profile> findByName(@Param("name") String name);
 }
