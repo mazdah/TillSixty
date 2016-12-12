b11b6bec3683423fb63d30b5071ded07b8634aea
Mazdah <woohj70@gmail.com>
2016-12-12 18:50:33 +0900

    @RepositoryRestResource에서 REST path를 "elements"로 설정

--- src/main/java/com/mazdah/tillsixty/goal/domain/ElementsRepository.java
+++ src/main/java/com/mazdah/tillsixty/goal/domain/ElementsRepository.java
@@ -1,7 +1,9 @@
 package com.mazdah.tillsixty.goal.domain;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="elements")
 public interface ElementsRepository extends MongoRepository<Elements, String> {
 
 }
