f7f781ff2654ffc781dceb6babdf089569128cdc
Mazdah <woohj70@gmail.com>
2016-12-13 18:59:26 +0900

    search 함수 추가

--- src/main/java/com/mazdah/tillsixty/goal/domain/ElementsRepository.java
+++ src/main/java/com/mazdah/tillsixty/goal/domain/ElementsRepository.java
@@ -1,9 +1,13 @@
 package com.mazdah.tillsixty.goal.domain;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path="elements")
 public interface ElementsRepository extends MongoRepository<Elements, String> {
-
+	List<Elements> findByUserIdAndGoalId(@Param("userId") String userId, @Param("goalId") String goalId);
+	List<Elements> findByUserIdAndGoalIdAndElementType(@Param("userId") String userId, @Param("goalId") String goalId, @Param("elementType") String elementType);
 }
